374bc5fcb2a4384127ffbe3bc820dbf8c65b22c8
KK <gkclove@gmail.com>
2022-01-12 16:52:01 +0800

    💥 feat(compiler): add 'routes' option

--- config/routes.ts
+++ config/routes.ts
@@ -25,6 +25,55 @@
     component: './Welcome',
   },
   {
+    path: '/config',
+    name: '配置管理',
+    icon: 'setting',
+    routes: [
+      {
+        //数据源管理
+        name: '数据源管理',
+        path: '/config/datasource',
+        component: './config/datasource',
+      },
+      {
+        //维度管理
+        name: '维度管理',
+        path: '/config/dimension',
+        component: './config/dimension',
+      },
+      {
+        //指标管理
+        name: '指标管理',
+        path: '/config/index',
+        component: './config/index',
+      },
+      {
+        //图表管理
+        name: '图表管理',
+        path: '/config/widget',
+        component: './config/widget',
+      },
+      {
+        name: '图表设计',
+        path: '/config/visualizer',
+        hideInMenu: true,
+        component: './config/visualizer',
+      },
+      {
+        //模板管理
+        name: '模板管理',
+        path: '/config/template',
+        component: './config/template',
+      },
+      {
+        //主题管理
+        name: '主题管理',
+        path: '/config/subject',
+        component: './config/subject',
+      },
+    ],
+  },
+  {
     path: '/admin',
     name: 'admin',
     icon: 'crown',
